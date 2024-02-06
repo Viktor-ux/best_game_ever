c882883fbd18a21feaf1566f83b8f8d4b55208a6
Viktor Feoktistov <fvs_2011@mail.ru>
2024-02-06 16:43:25 +0700

    Создан макет игры Space invaders

---
+++ bestgame.py
@@ -0,0 +1,20 @@
+import pygame
+import sys
+
+def run():
+
+    pygame.init()
+    screen = pygame.display.set_mode((1200, 800))
+    pygame.display.set_caption("Space Invaders")
+    bg_color = (0, 0, 0)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        screen.fill(bg_color)
+        pygame.display.flip()
+
+
+run()
